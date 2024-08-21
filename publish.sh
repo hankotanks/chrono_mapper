a9d419371e5142388817e2e8ee5134382f44b86a
hlewish3 <hank@modernatomic.com>
2024-08-21 06:47:49 -0600

    modification to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,12 @@
-wasm-pack build --target web --no-pack --out-di   r ./pkg
+wasm-pack build --target web --no-pack --out-dir ./pkg
 rm "./pkg/.gitignore"
 cp -r "./static"* "./pkg"
 cp -r "./js"* "./pkg"
 git switch --orphan gh-pages
 git checkout gh-pages
 git checkout master -- pkg
+git checkout gh-pages
 git add ./pkg
 git commit -m "deployment"
+git push
+sleep 50
