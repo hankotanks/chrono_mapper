25a8047d14495cab78f09f81e24de92f14e6542d
hlewish3 <hank@modernatomic.com>
2024-08-21 07:42:50 -0600

    removed old wasm library

--- histomap_bg.wasm.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function __wbg_test_free(a: number, b: number): void;
-export function test_hello(a: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_free(a: number, b: number, c: number): void;
