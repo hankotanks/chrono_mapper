3d2b0d683d3644a3df23d3bbe373f0e7e38d1780
hlewish3 <hank@modernatomic.com>
2024-09-20 04:08:23 -0600

    update publication script

--- publish.sh
+++ publish.sh
@@ -1,33 +1,16 @@
-current=$(git rev-parse --abbrev-ref HEAD)
-if [ "$current" != "master" ]; then
-  git stash
-  git checkout master
-fi
 cd app
 wasm-pack build --target web --no-pack --out-name core --out-dir ../pkg
 cd ..
 rm "./pkg/.gitignore"
 cp -r static/* pkg
 cp -r js/* pkg
-git add pkg/\*
-if [ $# -eq 0 ]; then
-    git commit -m "deployment from master"
-else
-  git commit -m "$1"
-fi
+git add -f pkg/\*
+git commit -m "temp"
 git checkout gh-pages
-git pull --rebase
 git checkout master -- pkg/*
-git checkout gh-pages
-mv ./pkg/{.,}* ./
+mv ./pkg/{.[!.],}* ./
 git add -A
 git commit -m "deployment"
 git push
 git checkout master
 git reset HEAD~
-echo "*" > "./pkg/.gitignore"
-git checkout $current
-if [ "$current" != "master" ]; then
-  git checkout $current
-  git stash pop
-fi
