facbae34017fbaf53f4503565252f0682d5b2192
hlewish3 <hank@modernatomic.com>
2024-10-03 04:19:22 -0600

    update publication script

--- publish.sh
+++ publish.sh
@@ -4,9 +4,11 @@ cd ..
 rm "./pkg/.gitignore"
 cp -r static/* pkg
 cp -r js/* pkg
+cp -r features pkg
 git add -f pkg/\*
 git commit -m "temp"
 git checkout gh-pages
+rm -R -- */
 git checkout master -- pkg/*
 mv ./pkg/{.[!.],}* ./
 git add -A
