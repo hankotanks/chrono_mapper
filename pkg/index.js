cde65cc39a31d66d08c145cc395139be8867f6f1
hlewish3 <hank@modernatomic.com>
2024-10-03 04:10:21 -0600

    temp

---
+++ pkg/index.js
@@ -0,0 +1,21 @@
+import init, { Wrapper } from "./core.js";
+
+// Initialization
+const exec = async () => {
+    Wrapper.run();
+
+    const resizeCanvas = _ => {
+        Wrapper.update_canvas(String(window.innerWidth), String(window.innerHeight));
+    };
+
+    resizeCanvas();
+
+    let sinceLastResize;
+    window.onresize = _ => {
+        clearTimeout(sinceLastResize);
+    
+        sinceLastResize = setTimeout(resizeCanvas, 300);
+    };
+};
+
+(async () => { await init(); exec(); })();
