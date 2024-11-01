d32fbaa9399da04ec32f29da553ea8e9950d7071
hlewish3 <hank@modernatomic.com>
2024-11-01 16:31:41 -0600

    learning how to copy arrays into WebAssembly.Memory

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,13 @@ const alloc = std.heap.wasm_allocator;
 const zjb = @import("zjb");
 const log = @import("log.zig");
 
-const manifest = @import("manifest");
-
 export fn getFeatures() callconv(.C) zjb.Handle {
+    const manifest = @import("manifest");
+    // instantiate a JS array object
     const list = zjb.global("Array").new(.{});
     for (manifest.assets) |asset| {
+        // this method returns the length of the array
+        // which we can safely discard
         _ = list.call("push", .{zjb.string(asset)}, i32);
     }
     return list;
@@ -16,12 +18,42 @@ comptime {
     zjb.exportFn("getFeatures", getFeatures);
 }
 
-export fn plotPoint(ctx: zjb.Handle, x: f32, y: f32, w: f32, h: f32) callconv(.C) void {
-    defer ctx.release();
-    const x0 = (x + 180.0) * w / 360.0;
-    const y0 = h - (y + 90.0) * h / 180.0;
-    ctx.call("fillRect", .{ x0, y0, 5, 5 }, void);
+export fn allocArray(n: i32) callconv(.C) i32 {
+    const ptr = (alloc.alloc(u32, @intCast(n))) catch {
+        return -1;
+    };
+    return @intCast(@intFromPtr(&ptr[0]));
 }
 comptime {
-    zjb.exportFn("plotPoint", plotPoint);
+    zjb.exportFn("allocArray", allocArray);
+}
+
+fn castSlice(comptime T: type, p: i32, n: i32) []T {
+    const length = @as(usize, @intCast(n));
+    const temp: usize = @bitCast(p);
+    return @as([*]T, @ptrFromInt(temp))[0..length];
+}
+
+export fn plotFeature(
+    ctx: zjb.Handle,
+    point_offset: i32,
+    point_count: i32,
+    ring_offset: i32,
+    ring_count: i32,
+    canvas_width: f32,
+    canvas_height: f32,
+) void {
+    defer ctx.release();
+    const points = castSlice(f32, point_offset, point_count);
+    defer alloc.free(points);
+    const rings = castSlice(u32, ring_offset, ring_count);
+    defer alloc.free(rings);
+    for (0..(points.len / 2)) |i| {
+        const x = (points[i * 2] + 180.0) * canvas_width / 360.0;
+        const y = canvas_height - (points[i * 2 + 1] + 90.0) * canvas_height / 180.0;
+        ctx.call("fillRect", .{ x, y, 5, 5 }, void);
+    }
+}
+comptime {
+    zjb.exportFn("plotFeature", plotFeature);
 }
--- web/index.js
+++ web/index.js
@@ -13,51 +13,55 @@ const loadExports = async (name, initial) => {
     const module = await WebAssembly.instantiateStreaming(request, params);
     zjb.setInstance(module.instance);
 
-    return zjb.exports;
+    return {
+        memory: module.instance.exports.memory,
+        exports: zjb.exports,
+    };
 };
 
 const parseFeatures = data => {
-    if (!('type' in data)) throw new Error();
+    // object MUST have a type AND type MUST be a FeatureCollection
+    if (!('type' in data)) throw new Error('Failed to parse feature layer');
     const type = data['type'];
-    if (type != 'FeatureCollection') throw new Error();
-
-    if (!('features' in data)) throw new Error();
+    if (type != 'FeatureCollection') throw new Error('Failed to parse feature layer');
+    // feature collecton must have an array of constituent features
+    if (!('features' in data)) throw new Error('Failed to parse feature layer');
     const features = [];
     for (const entry of data['features']) {
+        // feature MUST have a properties field
         if (!('properties' in entry)) continue;
         const properties = entry['properties'];
-
+        // AND it MUST contain a non-null name field
         if (!('NAME' in properties)) continue;
         const name = properties['NAME'];
         if (name == null) continue;
-
+        // feature MUST have geometry with a set of coordinates
         if (!('geometry' in entry)) continue;
         const geometry = entry['geometry'];
         if (!('type' in geometry)) continue;
         if (geometry['type'] != 'MultiPolygon') continue;
-
         if (!('coordinates' in geometry)) continue;
         const coordinates = geometry['coordinates'];
         if (coordinates.length == 0) continue;
-
+        // restructure feature
         const temp = {
             "name": name,
             "geometry": [],
         };
-
+        // populate parsed feature
         for (const ring of coordinates) {
             temp.geometry.push(ring.flat(2));
         }
-
+        // add to layer
         features.push(temp);
     }
-
+    // return null if the feature layer is empty, otherwise return it
     if (features.length == 0) return null;
     return features;
 };
 
 const init = async _ => {
-    const exports = await loadExports('core.wasm', 10);
+    const { exports, memory } = await loadExports('core.wasm', 10);
 
     const canvas = document.querySelector('body > canvas');
 
@@ -67,43 +71,55 @@ const init = async _ => {
         event.preventDefault();
     };
 
+    var selectedFeature;
     for (const featurePath of exports.getFeatures()) {
         let temp = document.createElement('button');
+        featureList.appendChild(temp);
 
         temp.appendChild(document.createTextNode(featurePath));
         temp.onclick = _ => {
             const url = `${location.href}${featurePath}`;
             fetch(url).then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    temp.style.backgroundColor = 'lightpink';
-                    throw new Error(`Request failed [${response.status}]`);
-                }
-            }).then(data => {
-                try {
-                    const featureData = parseFeatures(data);
-                    const ctx = canvas.getContext('2d');
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    for (const feature of featureData) {
-                        const points = feature.geometry.flat(1);
-                        for (let i = 0; i < points.length / 2; i++) {
-                            const x = points[i * 2];
-                            const y = points[i * 2 + 1];
-                            exports.plotPoint(ctx, x, y, canvas.width, canvas.height);
-                        }
+                if (response.ok) { return response.json(); }
+                throw new Error(`Request failed [${response.status}]`);
+            }).then(parseFeatures).then(features => {
+                if (!features.length) return;
+                const ctx = canvas.getContext('2d');
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                for (const feature of features) {
+                    const ringIndices = [0];
+                    for (const ring of feature.geometry) {
+                        ringIndices.push(ringIndices[ringIndices.length - 1] + ring.length);
                     }
-                    temp.style.background = 'transparent';
-                } catch (err) {
-                    throw new Error('Failed to parse feature layer');
+
+                    const points = feature.geometry.flat(1)
+                    const pointByteOffset = exports.allocArray(points.length);
+                    const pointsView = new Float32Array(memory.buffer, pointByteOffset, points.length);
+                    pointsView.set(points);
+
+                    const idxByteOffset = exports.allocArray(ringIndices.length);
+                    const idxView = new Uint32Array(memory.buffer, idxByteOffset, ringIndices.length);
+                    idxView.set(ringIndices);
+
+                    exports.plotFeature(
+                        ctx, 
+                        pointByteOffset, 
+                        points.length, 
+                        idxByteOffset, 
+                        ringIndices.length, 
+                        canvas.width, canvas.height,
+                    );
                 }
+                temp.style.background = 'darkseagreen';
+                if (selectedFeature) {
+                    selectedFeature.style.background = 'transparent';
+                }
+                selectedFeature = temp;
             }).catch(error => {
                 temp.style.backgroundColor = 'lightpink';
                 console.error(`${featurePath}:\n${error}`);
             });
         };
-
-        featureList.appendChild(temp);
     }
     
     const resizeCanvas = _ => {
@@ -116,7 +132,6 @@ const init = async _ => {
     let sinceLastResize;
     window.onresize = _ => {
         clearTimeout(sinceLastResize);
-    
         sinceLastResize = setTimeout(resizeCanvas, 250);
     };
 };
