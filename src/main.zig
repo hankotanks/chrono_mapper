249960a7724bf5dc956b728e07cd047d56c8a1bf
hlewish3 <hank@modernatomic.com>
2024-11-07 05:14:30 -0700

    moved JSON parsing to WASM

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,28 @@
 const std = @import("std");
+const json = std.json;
 const alloc = std.heap.wasm_allocator;
 const zjb = @import("zjb");
 const log = @import("log.zig");
+const geom = @import("geom.zig");
 
-export fn getFeatures() callconv(.C) zjb.Handle {
+export fn allocArrayBuffer(n: i32, bytes_per_element: i32) callconv(.C) i32 {
+    const size: usize = @intCast(n * bytes_per_element);
+    const ptr = alloc.alloc(u8, size) catch {
+        return -1;
+    };
+    return @intCast(@intFromPtr(&ptr[0]));
+}
+comptime {
+    zjb.exportFn("allocArrayBuffer", allocArrayBuffer);
+}
+
+fn sliceArrayBuffer(comptime T: type, p: i32, n: i32) []T {
+    const length = @as(usize, @intCast(n));
+    const temp: usize = @bitCast(p);
+    return @as([*]T, @ptrFromInt(temp))[0..length];
+}
+
+export fn getLayers() callconv(.C) zjb.Handle {
     const manifest = @import("manifest");
     // instantiate a JS array object
     const list = zjb.global("Array").new(.{});
@@ -15,45 +34,59 @@ export fn getFeatures() callconv(.C) zjb.Handle {
     return list;
 }
 comptime {
-    zjb.exportFn("getFeatures", getFeatures);
+    zjb.exportFn("getLayers", getLayers);
 }
 
-export fn allocArray(n: i32) callconv(.C) i32 {
-    const ptr = (alloc.alloc(u32, @intCast(n))) catch {
-        return -1;
-    };
-    return @intCast(@intFromPtr(&ptr[0]));
-}
-comptime {
-    zjb.exportFn("allocArray", allocArray);
-}
-
-fn castSlice(comptime T: type, p: i32, n: i32) []T {
-    const length = @as(usize, @intCast(n));
-    const temp: usize = @bitCast(p);
-    return @as([*]T, @ptrFromInt(temp))[0..length];
+inline fn parseLayer(offset: i32, count: i32) !geom.FeatureLayer {
+    // get slice containing JSON
+    const layer = sliceArrayBuffer(u8, offset, count);
+    defer alloc.free(layer);
+    // attempt to parse
+    const parsed = try json.parseFromSlice(geom.FeatureLayer, alloc, layer, .{});
+    // on success, return the parsed FeatureLayer
+    return parsed.value;
 }
 
-export fn plotFeature(
+fn renderLayerInner(
     ctx: zjb.Handle,
-    point_offset: i32,
-    point_count: i32,
-    ring_offset: i32,
-    ring_count: i32,
+    offset: i32,
+    count: i32,
     canvas_width: f32,
     canvas_height: f32,
-) void {
+) !void {
     defer ctx.release();
-    const points = castSlice(f32, point_offset, point_count);
-    defer alloc.free(points);
-    const rings = castSlice(u32, ring_offset, ring_count);
-    defer alloc.free(rings);
-    for (0..(points.len / 2)) |i| {
-        const x = (points[i * 2] + 180.0) * canvas_width / 360.0;
-        const y = canvas_height - (points[i * 2 + 1] + 90.0) * canvas_height / 180.0;
-        ctx.call("fillRect", .{ x, y, 5, 5 }, void);
+    // try to parse the layer
+    const layer = try parseLayer(offset, count);
+    // for each polygon in the layer, render
+    for (layer.features) |feature| {
+        for (feature.coordinates) |poly| {
+            geom.renderPolygon(ctx, poly, canvas_width, canvas_height);
+        }
     }
 }
+
+export fn renderLayer(
+    ctx: zjb.Handle,
+    offset: i32,
+    count: i32,
+    canvas_width: f32,
+    canvas_height: f32,
+) i32 {
+    // execute failable render
+    renderLayerInner(
+        ctx,
+        offset,
+        count,
+        canvas_width,
+        canvas_height,
+    ) catch |err| {
+        // if parsing failed
+        log.logError(alloc, "{}", .{err});
+        return 1;
+    };
+    // return 0 on success
+    return 0;
+}
 comptime {
-    zjb.exportFn("plotFeature", plotFeature);
+    zjb.exportFn("renderLayer", renderLayer);
 }
--- web/index.js
+++ web/index.js
@@ -1,6 +1,9 @@
+// instantiate the zig-js bridge
 const zjb = new Zjb();
 
-const loadExports = async (name, initial) => {
+// helper function to load a WASM module with `initial` pages of memory
+const loadModule = async (name, initial) => {
+    // configure WASM memory
     const params = {
         zjb: zjb.imports,
         env: {
@@ -8,11 +11,13 @@ const loadExports = async (name, initial) => {
             __stack_pointer: 0,
         },
     };
-
+    // request the module
     const request = fetch(name);
+    // init WASM
     const module = await WebAssembly.instantiateStreaming(request, params);
+    // configure ZJB
     zjb.setInstance(module.instance);
-
+    // return memory buffer and ZJB exports
     return {
         memory: module.instance.exports.memory,
         exports: zjb.exports,
@@ -46,12 +51,8 @@ const parseFeatures = data => {
         // restructure feature
         const temp = {
             "name": name,
-            "geometry": [],
+            "coordinates": coordinates,
         };
-        // populate parsed feature
-        for (const ring of coordinates) {
-            temp.geometry.push(ring.flat(2));
-        }
         // add to layer
         features.push(temp);
     }
@@ -61,59 +62,70 @@ const parseFeatures = data => {
 };
 
 const init = async _ => {
-    const { exports, memory } = await loadExports('core.wasm', 10);
+    // instantiate WASM module
+    const { exports, memory } = await loadModule('core.wasm', 10);
 
+    // get handle to canvas and its context
     const canvas = document.querySelector('body > canvas');
+    const canvasContext = canvas.getContext('2d');
 
+    // helper function to send a feature layer to zig
+    const renderLayer = layer => {
+        // encode as a Uint8Array
+        const layerArr = (new TextEncoder()).encode(JSON.stringify(layer));
+        // request WASM memory to store the Uint8Array
+        const layerByteOffset = exports.allocArrayBuffer(layerArr.length, 1);
+        // create a window of sufficient size into WASM memory
+        const layerView = new Uint8Array(memory.buffer, layerByteOffset, layerArr.length);
+        // write feature layer to WASM memory
+        layerView.set(layerArr);
+        // render the feature layer
+        return exports.renderLayer(
+            canvasContext,
+            layerByteOffset,
+            layerArr.length,
+            canvas.width, 
+            canvas.height,
+        );
+    };
+
+    // configure scrolling feature layer selector
     const featureList = document.getElementById('features');
     featureList.onwheel = event => {
         featureList.scrollLeft += event.deltaY;
         event.preventDefault();
     };
 
+    // populate feature layer selector
     var selectedFeature;
-    for (const featurePath of exports.getFeatures()) {
+    for (const featurePath of exports.getLayers()) {
+        // create a button for the current feature path
         let temp = document.createElement('button');
+        // append it to the DOM
         featureList.appendChild(temp);
-
+        // add label
         temp.appendChild(document.createTextNode(featurePath));
+        // handle render behavior
         temp.onclick = _ => {
             const url = `${location.href}${featurePath}`;
             fetch(url).then(response => {
                 if (response.ok) { return response.json(); }
-                throw new Error(`Request failed [${response.status}]`);
+                throw new Error(`Request failed [${response.status}].`);
             }).then(parseFeatures).then(features => {
+                // clear canvas if there are valid features to render
                 if (!features.length) return;
-                const ctx = canvas.getContext('2d');
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                for (const feature of features) {
-                    const ringIndices = [0];
-                    for (const ring of feature.geometry) {
-                        ringIndices.push(ringIndices[ringIndices.length - 1] + ring.length);
-                    }
-
-                    const points = feature.geometry.flat(1)
-                    const pointByteOffset = exports.allocArray(points.length);
-                    const pointsView = new Float32Array(memory.buffer, pointByteOffset, points.length);
-                    pointsView.set(points);
-
-                    const idxByteOffset = exports.allocArray(ringIndices.length);
-                    const idxView = new Uint32Array(memory.buffer, idxByteOffset, ringIndices.length);
-                    idxView.set(ringIndices);
-
-                    exports.plotFeature(
-                        ctx, 
-                        pointByteOffset, 
-                        points.length, 
-                        idxByteOffset, 
-                        ringIndices.length, 
-                        canvas.width, canvas.height,
-                    );
+                canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+                // render features
+                if (renderLayer({ "name": featurePath, "features": features })) {
+                    throw new Error('Unable to render feature layer.');
                 }
+                // mark feature layer as selected
                 temp.style.background = 'darkseagreen';
-                if (selectedFeature) {
+                // unselect previous feature layer
+                if (selectedFeature && !selectedFeature.isEqualNode(temp)) {
                     selectedFeature.style.background = 'transparent';
                 }
+                // store current selection
                 selectedFeature = temp;
             }).catch(error => {
                 temp.style.backgroundColor = 'lightpink';
@@ -122,13 +134,19 @@ const init = async _ => {
         };
     }
     
+    // helper function to resize canvas
     const resizeCanvas = _ => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+        if (selectedFeature) {
+            selectedFeature.click();
+        }
     };
 
+    // resize once on load
     resizeCanvas();
 
+    // resize canvas whenever the viewport dimensions change
     let sinceLastResize;
     window.onresize = _ => {
         clearTimeout(sinceLastResize);
